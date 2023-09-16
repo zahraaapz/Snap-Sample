07dcc0337e8872d444e316bfb0e5c877627b8b8d
zahraaapz <zahrapirooz1379@gmail.com>
2023-09-16 21:15:12 +0330

    snap example

--- lib/screen/map_screen.dart
+++ lib/screen/map_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:snap_example/constant/dimens.dart';
 import 'package:snap_example/constant/text_style.dart';
+import 'package:snap_example/gen/assets.gen.dart';
 import '../widget/widget.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -22,27 +26,49 @@ class CurrentwidgetStates {
 
 class _MyHomePageState extends State<MyHomePage> {
   List currentWidgetList = [CurrentwidgetStates.stateSelectOrgin];
+  List<GeoPoint> geoPoint = [];
+  Widget markerIcon = SvgPicture.asset(
+    Assets.icons.origin,
+    height: 100,
+    width: 48,
+  );
+
+  MapController mapController = MapController(
+     initPosition:
+          GeoPoint(latitude: 35.7367516373, longitude: 51.2911096718));
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
           body: Stack(
         children: [
-          Container(
-            color: Colors.grey,
+          SizedBox.expand(
+            child: OSMFlutter(
+                mapIsLoading:
+                    SpinKitCircle(color: Color.fromARGB(255, 2, 207, 36)),
+                controller: mapController,
+                osmOption: OSMOption(
+                    isPicker: true,
+                    markerOption: MarkerOption(
+                        advancedPickerMarker: MarkerIcon(
+                      iconWidget: markerIcon,
+                    )),
+                    zoomOption: ZoomOption(
+                        stepZoom: 1,
+                        maxZoomLevel: 18,
+                        minZoomLevel: 8,
+                        initZoom: 15))),
           ),
           currentWidget(),
           MyBackButton(
             onPressed: () {
-   if (currentWidgetList.length>1) {
-  setState(() {
-    currentWidgetList.removeLast();
-  });
-}
-
+              if (currentWidgetList.length > 1) {
+                setState(() {
+                  currentWidgetList.removeLast();
+                });
+              }
             },
           ),
-         
         ],
       )),
     );
@@ -63,8 +89,8 @@ class _MyHomePageState extends State<MyHomePage> {
       case CurrentwidgetStates.stateSelectDriver:
         widget = driver();
         break;
-        
     }
+   
     return widget;
   }
 
@@ -76,11 +102,21 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Padding(
           padding: EdgeInsets.all(Dimens.large),
           child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  currentWidgetList.add(CurrentwidgetStates.stateSelectDestination);
+              onPressed: () async {
+                GeoPoint originGeopoint = await mapController
+                    .getCurrentPositionAdvancedPositionPicker();
+                geoPoint.add(originGeopoint);
 
+                markerIcon = SvgPicture.asset(
+                  Assets.icons.destination,
+                  height: 100,
+                  width: 48,
+                );
+                setState(() {
+                  currentWidgetList
+                      .add(CurrentwidgetStates.stateSelectDestination);
                 });
+                mapController.init();
               },
               child: Text(
                 'انتخاب مبدا',
@@ -91,16 +127,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Positioned distination() {
     return Positioned(
-       bottom: 0,
+        bottom: 0,
         left: 0,
         right: 0,
         child: Padding(
           padding: EdgeInsets.all(Dimens.large),
           child: ElevatedButton(
               onPressed: () {
-                   setState(() {
+                setState(() {
                   currentWidgetList.add(CurrentwidgetStates.stateSelectDriver);
-
                 });
               },
               child: Text(
